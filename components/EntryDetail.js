73f6d90f008af0565b32b740f989276aee974399
abdomokhtar <abdel@incredible-qr.com>
2017-12-25 14:47:15 +0800

    - feat: Entry detail wiring

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
-import {View, ScrollView, Text, TouchableOpacity, Platform, StyleSheet} from 'react-native';
+import {View, Text, TouchableOpacity, Platform, StyleSheet} from 'react-native';
+import {connect} from 'react-redux';
+import {red, orange, blue,lightPurple, pink, purple, white} from '../utils/colors';
+import MetricCard from './MetricCard';
 
 class EntryDetail extends Component {
 
     render () {
         return (
-          <View>
-              <Text>
-                  Entry Detail View
-              </Text>
+          <View style={styles.container}>
+              <MetricCard metrics={this.props.metrics}/>
           </View>
         )
     }
 }
 
-export default EntryDetail;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: white,
+        padding: 20
+    }
+});
+
+const mapStateToProps = (entries, {navigation}) => {
+  const key = navigation.state.params.entryId;
+  return{
+      metrics: entries[key]
+  }
+};
+
+export default connect(mapStateToProps)(EntryDetail);
